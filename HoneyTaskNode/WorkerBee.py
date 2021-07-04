f316ba409e760c99490ec4583581c1091e64e306
yadav19 <dhiraj.yadav2873@gmail.com>
2021-07-04 21:52:29 +0530

    final Push After Presentation

--- HoneyTaskNode/WorkerBee.py
+++ HoneyTaskNode/WorkerBee.py
@@ -3,25 +3,32 @@ import socket
 import time
 import random
 import psutil as pt
+
+
+
+#Rigged Task for performance analysis
+#all tasks are homogenous 
 def dotask():
     print("doing task")
     counter = 1001
     fib = [0]*counter
     fib[1] = 1
-    # for i in range(counter):
-    #     for j in range(counter):
     for k in range(counter):
         fib[k] = (fib[k-1]+fib[k-2])
-    slp = 2 #random.randint(1,4)
-    print("Sleeping for",slp)
-    time.sleep(slp)
+    slp = 1
+    time.sleep(slp) # for extra latency
 
+#Current CPU Power calculation
 def powerCalc():
     cpuStat =  pt.virtual_memory()
     return cpuStat.available/cpuStat.total
 
+
+# main program starts here
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("localhost",9952))
+s.connect(("192.168.43.133",9954))
+
+#scout response
 pow = f"{powerCalc()}"
 s.send(pow.encode()) #power of cpu
 print(s,pow)
@@ -43,13 +50,16 @@ def listener(s):
 threading.Thread(target = listener,daemon=True,args=(s,)).start()
 while True:
         data = s.recv(1024).decode()
+
         if data =="Power":
             pow = f"Power:{powerCalc()}"
             print(pow)
             s.send(pow.encode())
             print("power sent")
+        
         elif data == "Status":
             s.send(b"Running")
+        
         elif data == "Task":
             print("task!!!")
             t = threading.Thread(target = dotask,daemon = True)
